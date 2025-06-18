c59ca8310b4f1a940200bfb40d86a89b0a046fa5
K DANUSAN <danusan.official@gmail.com>
2025-06-18 12:31:26 +0530

    Define State Variables
    use default export
    use react hooks - use state
    module - mock data
    sub module, testcase =  null

--- project/src/pages/MyTestCasePage.tsx
+++ project/src/pages/MyTestCasePage.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function MyTestCasePage() {
+const MyTestCasePage = () => {
+  const [modules, setModules] = useState([
+    { id: 1, name: 'Module 1', subModules: [{ id: 'a', name: 'Sub Module 1' }, { id: 'b', name: 'Sub Module 2' }] },
+    { id: 2, name: 'Module 2', subModules: [{ id: 'c', name: 'Sub Module 3' }, { id: 'd', name: 'Sub Module 4' }] },
+  ]);
+  const [selectedModule, setSelectedModule] = useState<any>(null);
+  const [selectedSubModule, setSelectedSubModule] = useState<any>(null);
+  const [testCases, setTestCases] = useState<any[]>([]);
+
   return (
     <div>
-      dfgdfgdfgf
+      <h1>Test Case Management</h1>
     </div>
-  )
-}
+  );
+};
 
-export default MyTestCasePage
+export default MyTestCasePage;
+
