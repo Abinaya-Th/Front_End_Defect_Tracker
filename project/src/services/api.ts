b4026343f76087de5558d8f56582dfb1fbae9d0e
Nishakaran <Mathavarasa.Nishakaran@gmail.com>
2025-06-25 13:24:39 +0530

    Relese Search api integrate final

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://192.168.1.99:8083/api/v1';
+const API_BASE = 'http://192.168.1.99:8085/api/v1';
 
 export const searchReleaseByName = async (releaseName: string) => {
     const response = await axios.get(`${API_BASE}/releases/search`, {
