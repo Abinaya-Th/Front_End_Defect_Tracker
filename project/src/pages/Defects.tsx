43b4e72d633c71c68bc7ee29c548afa11b4d0af4
Niluxshan07 <sniluxshan@std.appsc.sab.ac.lk>
2025-07-09 17:31:02 +0530

    nilux

--- project/src/pages/Defects.tsx
+++ project/src/pages/Defects.tsx
@@ -1329,6 +1329,7 @@ console.log(releasesData, "Release Data");
                 onChange={e => handleInputChange('typeId', e.target.value)}
                 className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
                 required
+                //nilux
               >
                 <option value="">Select type</option>
                 {defectTypes.map((t) => (
