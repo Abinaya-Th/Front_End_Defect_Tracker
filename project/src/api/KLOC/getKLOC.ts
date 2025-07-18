7d8c9985f865fccd2df84b19414d8e6e5c0fb9db
Sathursanak <132430609+Sathursanak@users.noreply.github.com>
2025-07-18 08:46:23 +0530

    icon change

--- project/src/api/KLOC/getKLOC.ts
+++ project/src/api/KLOC/getKLOC.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Fetch defect density (KLOC and defect count) for a given project
 export async function getDefectDensity(projectId: string) {
-  const url = `http://192.168.1.107:8080/api/v1/dashboard/defect-density/${projectId}`;
+  const url = `http://34.171.115.156:8087/api/v1/dashboard/defect-density/${projectId}`;
   const response = await axios.get(url);
   return response.data;
 }
--- project/src/api/dashboard/dash_get.ts
+++ project/src/api/dashboard/dash_get.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Fetch defect severity summary for a given project
 export async function getDefectSeveritySummary(projectId: string) {
-  const url = `http://192.168.1.107:8080/api/v1/dashboard/defect_severity_summary/${projectId}`;
+  const url = `http://34.171.115.156:8087/api/v1/dashboard/defect_severity_summary/${projectId}`;
   const response = await axios.get(url);
   return response.data;
 }
